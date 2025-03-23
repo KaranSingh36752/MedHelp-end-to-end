302e1f715fbac9fec64cdccbebb1b6c92f3f54c1
ishpreet <ishpreet36752.com>
2025-03-23 15:03:30 +0530

    set up

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name= 'MedHelp Generative AI Project',
+    version= '0.0.0',
+    author= 'IShpreet Singh',
+    author_email='ishpreet36752@gmail.com',
+    packages=find_packages(),
+    insatll_requires = []
+)
